c889be174a1614cddb64f2d5fc9d2e2a83a4485f
= <=>
2022-12-13 16:51:39 +0100

    dawdasd

--- zadaci/forum/php/novaSlika.php
+++ zadaci/forum/php/novaSlika.php
@@ -1,36 +1,40 @@
 <?php
+session_start();
 
-// Get reference to uploaded image
 $image_file = $_FILES["fajl"];
 
-// Exit if no file uploaded
 if (!isset($image_file)) {
     die('No file uploaded.');
 }
 
-// Exit if image file is zero bytes
 if (filesize($image_file["tmp_name"]) <= 0) {
     die('Uploaded file has no contents.');
 }
 
-// Exit if is not a valid image file
 $image_type = exif_imagetype($image_file["tmp_name"]);
 if (!$image_type) {
     die('Uploaded file is not an image.');
 }
 
-// Get file extension based on file type, to prepend a dot we pass true as the second parameter
 $image_extension = image_type_to_extension($image_type, true);
 
-// Create a unique image name
 $image_name = bin2hex(random_bytes(16)) . $image_extension;
 
-// Move the temp image file to the images directory
-move_uploaded_file(
-    // Temp image location
-    $image_file["tmp_name"],
+if(move_uploaded_file($image_file["tmp_name"], "../img/" . $_SESSION["ime"] . $image_extension)) {
+    echo "Uploadovano";
 
-    // New image location
-    __DIR__ . "//images//" . $image_name
-);
+    require "konekcija.php";
+    $sql = "UPDATE korisnici SET slika='" . $_SESSION["ime"] . $image_extension . "' WHERE emajl='" . $_SESSION["mejl"]."'";
+    if(mysqli_query($conn, $sql)) {
+        echo "ok";
+    }
+    else {
+        echo "nije ok";
+    }
+
+    /* echo "<script>alert('Uspesno uploadovana slika!');</script>";
+    header('refresh:0;url=../index.php');
+    exit(); */
+}
+else echo "Nije";
 ?>
